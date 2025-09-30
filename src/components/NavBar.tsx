4f9b362354eb8eacb9d0d2e8ed7cff387f611485
esther <olapadeesther83@gmail.com>
2025-09-30 14:57:09 +0100

    Initial commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+import logo from "../assets/logo.png";
+
+const NavBar = () => {
+  return (
+    <div>
+      <img src={logo} alt="Game Hub Logo" className="logo" />
+    </div>
+  );
+};
+
+export default NavBar;
