06bf1c0255b06a68299796e23f20b002b24c6049
esther <olapadeesther83@gmail.com>
2025-10-03 19:09:49 +0100

    get optimised-images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import type { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../components/image-url";
 
 interface Props {
   game: Game;
@@ -10,16 +11,21 @@ const GameCard = ({ game }: Props) => {
   return (
     <div className="game-card">
       <div className="game-image-container">
-        <img
-          src={game.background_image}
-          alt={game.name}
-          className="game-image"
-        />
+        {game.background_image ? (
+          <img
+            src={getCroppedImageUrl(game.background_image)}
+            alt={game.name}
+            className="game-image"
+          />
+        ) : (
+          <div className="no-image">No Image Available</div>
+        )}
       </div>
+
       <div className="game-card-body">
         <h3 className="game-title">{game.name}</h3>
 
-        <div logo-score>
+        <div className="logo-score">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
